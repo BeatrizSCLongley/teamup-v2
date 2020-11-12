cf6cd84c765a2ea062a39ce1177297beb7635d34
Beatriz Longley <beatrizsousacosta@gmail.com>
2020-11-12 16:07:09 +0100

    added production setup for postmark according to lw lecture

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,10 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method     = :postmark
+  config.action_mailer.postmark_settings   = { api_token: Rails.application.credentials.postmark_api_token }
+  config.action_mailer.default_url_options = { host: "www.teamup.inc" }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
