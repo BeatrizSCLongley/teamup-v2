81a466139c49026cbca68aa0eb9b27df7bc813f6
Beatriz Longley <beatrizsousacosta@gmail.com>
2020-11-12 16:20:10 +0100

    final adjustments

---
+++ config/initializers/smtp.rb
@@ -0,0 +1,9 @@
+# ActionMailer::Base.smtp_settings = {
+#   address: 'hello@teamup.inc',
+#   port: '25',
+#   domain: 'www.teamup.inc',
+#   user_name: Rails.application.credentials.postmark_api_token,
+#   password: Rails.application.credentials.postmark_api_token,
+#   authentication: :cram_md5,
+#   enable_starttls_auto: true
+# }
