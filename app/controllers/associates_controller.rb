c3e919f89e095e7790ac7f14956b9718382c4454
Beatriz Longley <beatrizsousacosta@gmail.com>
2020-12-11 11:12:11 +0100

    map is done, with some of the associates / markers inifity

--- app/controllers/associates_controller.rb
+++ app/controllers/associates_controller.rb
@@ -1,4 +1,14 @@
 class AssociatesController < ApplicationController
   def index
+    @associates = Associate.all
+
+    # the `geocoded` scope filters only associates with coordinates (latitude & longitude)
+    @markers = @associates.geocoded.map do |associate|
+      {
+        lat: associate.latitude,
+        lng: associate.longitude,
+        image_url: helpers.asset_url('loop_favicon_32x32.png')
+      }
+    end
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,9 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-// Bootstrap
+// External
 import 'bootstrap';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -20,12 +21,13 @@ import 'bootstrap';
 
 // -----------------------------------------------------
 // Import functions here:
-import { navbarScroll } from '../components/navbar';
 // import { downloadBtn } from '../components/download';
+import { navbarScroll } from '../components/navbar';
+import { initMapbox } from '../plugins/init_mapbox';
 
 document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
   // initUpdateNavbarOnScroll();
-  navbarScroll();
   // downloadBtn();
+  navbarScroll();
+  initMapbox();
 });
