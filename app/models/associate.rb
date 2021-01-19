dce9315da9b6fb6d3a10d879a96e0cc69e7890f9
Beatriz Longley <beatrizsousacosta@gmail.com>
2021-01-19 14:51:51 +0100

    changed photo to image on the model and seeds

--- app/models/associate.rb
+++ app/models/associate.rb
@@ -1,5 +1,5 @@
 class Associate < ApplicationRecord
-  has_one_attached :photo
+  has_one_attached :image
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
   # validates :name, :bio, :location, :latitude, :longitude, :image, presence: true
