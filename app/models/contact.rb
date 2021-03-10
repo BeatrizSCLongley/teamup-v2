0e72b5750a38b4aab6ae65ffb1a95ac36b0dc3f3
Beatriz Longley <beatrizsousacosta@gmail.com>
2021-03-10 11:50:36 +0100

    validation email

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ApplicationRecord
   belongs_to :solution
   validates :name, :email, presence: true
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: 'please enter a valid email address' }
 end
