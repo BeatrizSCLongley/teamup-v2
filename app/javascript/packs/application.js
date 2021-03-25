8b38178eef7d500bcaa767340d70acc336fb0f3d
Beatriz Longley <beatrizsousacosta@gmail.com>
2021-03-25 15:14:57 +0100

    aos js file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+require("components/aos_scrolling")
+
 // External
 import 'bootstrap';
 import 'mapbox-gl/dist/mapbox-gl.css';
